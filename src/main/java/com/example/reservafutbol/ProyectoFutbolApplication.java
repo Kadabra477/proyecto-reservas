6882c7fac64b0bd7665e2d54676ff315a52de15e
Hernan <hernandimichele477@gmail.com>
2025-05-26 17:26:26 -0300

    Probar endpoints

--- src/main/java/com/example/reservafutbol/ProyectoFutbolApplication.java
+++ src/main/java/com/example/reservafutbol/ProyectoFutbolApplication.java
@@ -2,11 +2,26 @@ package com.example.reservafutbol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 @SpringBootApplication
 public class ProyectoFutbolApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoFutbolApplication.class, args);
+	}
+
+	@Bean
+	public ApplicationRunner applicationRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("=== ENDPOINTS DISPONIBLES ===");
+			RequestMappingHandlerMapping mapping = ctx.getBean(RequestMappingHandlerMapping.class);
+			mapping.getHandlerMethods().forEach((key, value) -> {
+				System.out.println(key + " => " + value);
+			});
+		};
 	}
 }
